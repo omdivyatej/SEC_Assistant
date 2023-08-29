556a0a9a2fe689f2a1aef1a899cecb8dce100609
omdivyatej <od302002@gmail.com>
2023-08-29 13:15:51 +0530

    layout

--- SEC_Assistant/settings.py
+++ SEC_Assistant/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dashboard/static'),    
+    # Add other app-specific static directories as needed
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    # User Identification
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Personal Information
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
+    # Contact Information
+    email = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+
+    # User Activity and History
+    last_login = models.DateTimeField(null=True, blank=True)
+    signup_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username
