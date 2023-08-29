7b5bd480a4a34f725813af6b7bcaa19b74c3f1a2
omdivyatej <od302002@gmail.com>
2023-08-29 14:25:28 +0530

    layout

--- SEC_Assistant/settings.py
+++ SEC_Assistant/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'dashboard',
-    'bootstrap5',
+INSTALLED_APPS = [       
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard', 
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
